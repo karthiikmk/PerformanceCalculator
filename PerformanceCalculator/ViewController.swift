f2d26d7ab30b136b4d4ae5e25ea9b65c979ba505
Karthik <karthik.samy@anywhere.co>
2021-08-30 23:14:30 +0530

    Adds Minor code refactors and cleanups.

--- PerformanceCalculator/ViewController.swift
+++ PerformanceCalculator/ViewController.swift
@@ -1,57 +1,19 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import UIKit
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var msImage: UIImageView!
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
         columBreakPoint()
-	}
+    }
 
     func columBreakPoint() {
-
         let name: [String?] = ["Apple", "Samsung", "Microsoft", "Google", nil]
-
-
-
-
         let filtered = name.compactMap { $0 }
             .filter { $0.count <= 6 }
             .map { "Tech: \($0)" }
-
         print(filtered)
-
-
-
-
-
-
-
-
-
     }
 }
